48806c38ab4032c1112e851ec433787db9239258
Deiki <DeicPro@users.noreply.github.com>
2020-06-11 18:27:00 +0200

    Improve code with some changes
    - Import only parts needed of libraries to optimize the performance
    - Output errors to a log file
    - Added print as a variable instead of old sys.stdout.write for refresh print parts
    - Created a function for invalid input parameters
    - Changed progress bar style
    - Check if request is taking longer
    - Merge string functions in one function

--- getwarp.py
+++ getwarp.py
@@ -1,122 +1,135 @@
-import urllib.request
-import json
-import datetime
-import random
-import string
-import time
-import os
-import sys
-import pathlib
+from datetime import datetime
+from time import sleep
 
-script_version = '1.0.0'
+script_version = 'v1.1.7'
 script_title = f"GetWARP {script_version} by Deiki"
-os.system('title ' + script_title if os.name == 'nt' else 'PS1="\[\e]0;' + script_title + '\a\]"; echo $PS1')
-os.system('cls' if os.name == 'nt' else 'clear')
+
+from functools import partial
+tolog = partial(print, "[{}]".format(datetime.now()), file=open('getwarp.log', 'w'))
+printr = partial(print, end='', flush=True)
+
+from os import system, name
+system('title ' + script_title if name == 'nt' else 'PS1="\[\e]0;' + script_title + '\a\]"; echo $PS1')
+system('cls' if name == 'nt' else 'clear')
+
+print (f'''
+	<> {script_title} <>
+
+ [i] Get free WARP+ GB bandwidth for 1.1.1.1 app.
+
+ # GitHub: github.com/DeicPro/GetWARP
+ # Telegram: @Deiki
+''')
+
+def invalidParam(param):
+	print(f" [!] \'{param}\' is not a valid parameter.")
 
 def newID():
 	while True:
-		referrer  = input("[#] Enter your client ID\n > ")
+		referrer = input(" [#] Enter your client ID\n > ")
 		user_input = input(f" [?] ID = {referrer}, sure? [y/n]\n > ")
 		if user_input == "y":
 			save_id = input(" [?] Do you want to save your ID? [y/n]\n > ")
 			if save_id == "y":
-			    with open("referrer.txt","w") as file:
+			    with open("client_id.txt","w") as file:
 				    file.write(referrer)
 			    return referrer
 			elif save_id == "n":
 				return referrer
 			else:
-			    print(f"\"{save_id}\" is not a valid parameter.")
+			    invalidParam(save_id)
 		elif user_input == "n":
 			user_input = None
 		else:
-			print(f"\"{user_input}\" is not a valid parameter.")
+			invalidParam(user_input)
 
 def progressBar():
-	animation     = ["[□□□□□□□□□□]","[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]"]
+	animation = ["▱▱▱▱▱▱▱▱▱▱","▰▱▱▱▱▱▱▱▱▱","▰▰▱▱▱▱▱▱▱▱", "▰▰▰▱▱▱▱▱▱▱", "▰▰▰▰▱▱▱▱▱▱", "▰▰▰▰▰▱▱▱▱▱", "▰▰▰▰▰▰▱▱▱▱", "▰▰▰▰▰▰▰▱▱▱", "▰▰▰▰▰▰▰▰▱▱", "▰▰▰▰▰▰▰▰▰▱"]
 	progress_anim = 0
-	save_anim     = animation[progress_anim % len(animation)]
-	percent       = 0
+	save_anim = animation[progress_anim % len(animation)]
+	percent = 0
 	while True:
-		for i in range(10):
+		for _ in range(10):
 			percent += 1
-			sys.stdout.write(f"\r[+] Sending request...  " + save_anim + f" {percent}%")
-			sys.stdout.flush()
+			printr(f"\r Sending request...  " + save_anim + f" {percent}%")
 			if result.is_alive():
-				time.sleep(0.1)
+				sleep(0.1)
 			else:
-				time.sleep(0.005)
+				sleep(0.005)
 		progress_anim += 1
 		save_anim = animation[progress_anim % len(animation)]
-		if percent == 100:
-			sys.stdout.write("\r[+] Received response.  [■■■■■■■■■■] 100%")
+		if percent == 75 and result.is_alive == True:
+				while result.is_alive:
+					for _ in range(10):
+						printr(f"\r Sending request...  " + save_anim + f" {percent}% (it is taking longer than usual)")
+						sleep(1)
+					#result.abort()
+					break
+		elif percent == 100:
+			printr("\r Received response.  ▰▰▰▰▰▰▰▰▰▰ 100%")
 			break
 
-def genString(stringLength):
+from string import ascii_letters, digits
+from random import choice
+def genString(stringLength, charsType):
 	try:
-		letters = string.ascii_letters + string.digits
-		return ''.join(random.choice(letters) for i in range(stringLength))
+		chars = charsType
+		return ''.join(choice(chars) for i in range(stringLength))
 	except Exception as error:
-		print(error)		    
+		tolog(error)
 
-def digitString(stringLength):
-	try:
-		digit = string.digits
-		return ''.join((random.choice(digit) for i in range(stringLength)))    
-	except Exception as error:
-		print(error)
-
-url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
+url = f'https://api.cloudflareclient.com/v0a{genString(3, digits)}/reg'
 
 from threading import Thread
-
+from json import dumps
+from urllib import request
 class sendRequest(Thread):
 	def run(self):
 		try:
-			install_id  = genString(22)
-			body        = {"key": "{}=".format(genString(43)),
-					"install_id": install_id,
-					"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
-					"referrer": referrer,
-					"warp_enabled": False,
-					"tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
-					"type": "Android",
-					"locale": "zh-CN"}
-			data        = json.dumps(body).encode('utf8')
-			headers     = {'Content-Type': 'application/json; charset=UTF-8',
-					'Host': 'api.cloudflareclient.com',
-					'Connection': 'Keep-Alive',
-					'Accept-Encoding': 'gzip',
-					'User-Agent': 'okhttp/3.12.1'}
-			req         = urllib.request.Request(url, data, headers)
-			response    = urllib.request.urlopen(req)
-			status_code = response.getcode()
-			self.status = status_code
+			install_id = genString(22, ascii_letters)
+			body = {"key": "{}=".format(genString(43, ascii_letters)),
+				"install_id": install_id,
+				"fcm_token": "{}:APA91b{}".format(install_id, genString(134, ascii_letters)),
+				"referrer": referrer,
+				"warp_enabled": False,
+				"tos": datetime.now().isoformat()[:-3] + "+07:00",
+				"type": "Android",
+				"locale": "zh-CN"}
+			data = dumps(body).encode('utf8')
+			headers = {'Content-Type': 'application/json; charset=UTF-8',
+				'Host': 'api.cloudflareclient.com',
+				'Connection': 'Keep-Alive',
+				'Accept-Encoding': 'gzip',
+				'User-Agent': 'okhttp/3.12.1'}
+			req = request.Request(url, data, headers)
+			resp = request.urlopen(req)
+			self.status = resp.getcode()
 		except Exception as error:
+			tolog(error)
 			self.status = error
 
-if pathlib.Path("referrer.txt").exists():
+from pathlib import Path
+if Path("client_id.txt").exists():
 	while True:
 		user_input = input(" [?] Do you want to use saved client ID? [y/n]\n > ")
 		if user_input == "y":
-			with open("referrer.txt","r") as file:
+			with open("client_id.txt","r") as file:
 				referrer = file.read().strip()
 			break
 		elif user_input == "n":
 			referrer = newID()
 			break
 		else:
-			print(f"\"{user_input}\" is not a valid parameter.")
+			invalidParam(user_input)
 else:
 	referrer = newID()
 
 g = 0
-b = 0
+f = 0
+
 while True:
-	os.system('cls' if os.name == 'nt' else 'clear')
-	print("")
-	print(f"	<> {script_title} <>")
-	print("")
+	system('cls' if name == 'nt' else 'clear')
+	print(f"\n	<> {script_title} <>\n")
 	result = sendRequest()
 	result.start()
 	print(f" # Current ID: {referrer}")
@@ -124,17 +137,12 @@ while True:
 	if result.status == 200:
 		g += 1
 		print(f"\n\n [i] You've got {g} GB (failed: {f})")
-		print("")
 		for i in range(18,0,-1):
-			sys.stdout.write(f"\r * Sending a new request in {i} seconds...")
-			sys.stdout.flush()
-			time.sleep(1)
+			printr(f"\r * Sending a new request in {i} seconds...")
+			sleep(1)
 	else:
-		b += 1
-		print(f"\n\n [i] You've got {g} GB (failed: {f})\n")
-		print("")
-		print("[!] Has been occurred some problem when sent the request:\n" + str(result.status))
+		f += 1
+		print(f'''\n\n [i] You've got {g} GB (failed: {f})\n [!] Has been occurred some problem when sent the request:\n''' + str(result.status) + "\n")
 		for i in range(18,0,-1):
-			sys.stdout.write(f"\r * Retrying again in {i} seconds...")
-			sys.stdout.flush()
-			time.sleep(1)
+			printr(f"\r * Retrying again in {i} seconds...")
+			sleep(1)
